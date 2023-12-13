efd658c0d9f68daf7a75e4ada1376063601b649c
ravishkaw <127718357+ravishkaw@users.noreply.github.com>
2023-12-13 20:29:50 +0530

    add facebook download

---
+++ other-functions/facebook.js
@@ -0,0 +1,25 @@
+//npm i @xaviabot/fb-downloader
+const getFBInfo = require("@xaviabot/fb-downloader");
+
+const facebookDownload = async (msg, chat, MessageMedia, client) => {
+  chat.sendSeen();
+  msg.react("🔄️");
+  try {
+    let URL = msg.body;
+    const result = await getFBInfo(URL);
+    console.log(result);
+    const media = await MessageMedia.fromUrl(result.hd, {
+      unsafeMime: true,
+    });
+
+    msg.react("✅");
+    await msg.reply(new MessageMedia(media.mimetype, media.data, result.title));
+
+  } catch (err) {
+    console.log(err);
+    msg.react("❗");
+    msg.reply(err.toString());
+  }
+};
+
+module.exports.facebookDownload = facebookDownload;
