e64b53f268e9fb075ee6c3e69ef556d2e9b237a3
ravishkaw <127718357+ravishkaw@users.noreply.github.com>
2023-12-17 21:02:45 +0530

    update all functions and now working

--- web-js-functions/botinfo.js
+++ web-js-functions/botinfo.js
@@ -1,8 +1,5 @@
-// TODO: Add more info about the bot and link to index.js
-
 // Bibi Info
-async function bibiInfo() {
-
+const bibiInfo = async (msg, chat, client) => {
   chat.sendSeen();
   chat.sendStateTyping();
   let info = client.info;
@@ -17,71 +14,54 @@ async function bibiInfo() {
     );
     chat.clearState();
   }, 400);
-}
+};
 
-//!make async and link to index.js
-//  // Direct send a new message to owner
-//   if (msg.body.startsWith(".sendowner ")) {
-//   chat.sendSeen();
-//   chat.sendStateTyping();
-//   msg.react("🔄️");
-//   if (!chat.isGroup) {
-//     let msg1 = msg.body.slice(11);
-//     let number = `94775048662@c.us`;
-//     setTimeout(() => {
-//       msg.react("✅");
-//       msg.reply("Done. Sent to Ravishka");
-//       client.sendMessage(
-//         number,
-//         `${contact.pushname}\n${contact.id.user}\n\n${msg1}`
-//       );
-//       chat.clearState();
-//     }, 800);
-//   }
-// }
+// Direct send a new message to owner
+const sendOwner = async (msg, chat, client, contact) => {
+  chat.sendSeen();
+  chat.sendStateTyping();
+  msg.react("🔄️");
+  if (!chat.isGroup) {
+    let msg1 = msg.body.slice(11);
+    let number = `94775048662@c.us`;
+    setTimeout(() => {
+      msg.react("✅");
+      msg.reply("Done. Sent to Ravishka");
+      client.sendMessage(
+        number,
+        `${contact.pushname}\n${contact.id.user}\n\n${msg1}`
+      );
+      chat.clearState();
+    }, 800);
+  }
+};
 
-// // Direct send a new message to specific id
-// else if (msg.body.startsWith(".sendton ")) {
-//   chat.sendSeen();
-//   let number = msg.body.split(" ")[1];
-//   let messageIndex = msg.body.indexOf(number) + number.length;
-//   let msg2 = msg.body.slice(messageIndex, msg.body.length);
-//   number = number.includes("@c.us") ? number : `${number}@c.us`;
-//   setTimeout(() => {
-//     msg.react("✅");
-//     client.sendMessage(number, `🔰 *Message from Ravishka* 🔰\n\n${msg2}`);
-//   }, 500);
-// }
+// Direct send a new message to specific id
+const sendToNo = async (msg, chat, client) => {
+  chat.sendSeen();
+  let number = msg.body.split(" ")[1];
+  let messageIndex = msg.body.indexOf(number) + number.length;
+  let msg2 = msg.body.slice(messageIndex, msg.body.length);
+  number = number.includes("@c.us") ? number : `${number}@c.us`;
+  setTimeout(() => {
+    msg.react("✅");
+    client.sendMessage(number, `🔰 *Message from Ravishka* 🔰\n\n${msg2}`);
+  }, 500);
+};
 
-// //opened chats
-// else if (msg.body === ".chats") {
-//   chat.sendSeen();
-//   chat.sendStateTyping();
-//   const chats = await client.getChats();
-//   setTimeout(() => {
-//     msg.react("✅");
-//     client.sendMessage(msg.from, `I have ${chats.length} chats open.`);
-//     chat.clearState();
-//   }, 400);
-// }
-
-// //chat or group id
-// else if (msg.body === ".id") {
-//   chat.sendSeen();
-//   let id = chat.id.user;
-//   let number = `94775048662@c.us`;
-//   msg.react("✅");
-//   client.sendMessage(number, id);
-// }
-
-// //clear chat
-// else if (msg.body === ".clear") {
-//   if (!chat.isGroup) {
-//     chat.sendSeen();
-//     chat.clearMessages(true);
-//     msg.react("✅");
-//     chat.sendMessage("Done.");
-//   }
-// }
+//opened chats
+const chats = async (msg, chat, client) => {
+  chat.sendSeen();
+  chat.sendStateTyping();
+  const chats = await client.getChats();
+  setTimeout(() => {
+    msg.react("✅");
+    client.sendMessage(msg.from, `I have ${chats.length} chats open.`);
+    chat.clearState();
+  }, 400);
+};
 
 module.exports.bibiInfo = bibiInfo;
+module.exports.sendOwner = sendOwner;
+module.exports.sendToNo = sendToNo;
+module.exports.chats = chats;
