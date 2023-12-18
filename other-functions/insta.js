9fffccce72e4b531f0215d3dc3036f4d2163192f
ravishkaw <127718357+ravishkaw@users.noreply.github.com>
2023-12-18 08:17:51 +0530

    finalize insta module

--- other-functions/insta.js
+++ other-functions/insta.js
@@ -4,7 +4,7 @@ const getRandom = (ext) => {
   return `${Math.floor(Math.random() * 10000)}${ext}`;
 };
 
-const instagramDownload = async (msg, chat, MessageMedia, client) => {
+const instagramDownload = async (msg, chat, MessageMedia) => {
   chat.sendSeen();
   msg.react("🔄️");
   try {
@@ -12,7 +12,6 @@ const instagramDownload = async (msg, chat, MessageMedia, client) => {
     let Name = "IG Video " + getRandom(".mp4");
 
     const dataList = await instagramDl(URL);
-    // console.log(dataList[0].download_link);
     const media = await MessageMedia.fromUrl(dataList[0].download_link, {
       unsafeMime: true,
     });
